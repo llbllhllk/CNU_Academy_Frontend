d64d7a8584cdb48958b5b187f786527575842378
llbllhllk <rkdqudgus9806@gmail.com>
2023-07-05 12:49:18 +0900

    week8: Spacer Component

--- study-react/src/components/Spacer/index.js
+++ study-react/src/components/Spacer/index.js
@@ -9,11 +9,30 @@ const Spacer = ({ children, type = 'horizontal', size = 8, ...props }) => {
     verticalAlign: type === 'horizontal' ? 'middle' : undefined,
   }
 
+  const nodes = React.Children.toArray(children)
+    .filter((element) => React.isValidElement(element))
+    .map((element, index, elements) => {
+      return React.cloneElement(element, {
+        ...element.props,
+        style: {
+          ...element.props.style,
+          marginRight:
+            type === 'horizontal' && index !== elements.length - 1
+              ? size
+              : undefined,
+          marginBottom:
+            type === 'vertical' && index !== elements.length - 1
+              ? size
+              : undefined,
+        },
+      })
+    })
+
   return (
     <div {...props} style={spacerStyle}>
-      {children}
+      {nodes}
     </div>
   )
 }
 
-return Spacer
+export default Spacer
--- study-react/src/stories/Spacer.stories.js
+++ study-react/src/stories/Spacer.stories.js
@@ -2,14 +2,43 @@ import React from 'react'
 import Spacer from '../components/Spacer'
 
 export default {
-  title: 'Components/Spacer',
+  title: 'Component/Spacer',
   component: Spacer,
   argTypes: {
-    type: { control: string },
     size: { control: { type: 'range', min: 8, max: 64 } },
   },
 }
 
-export const Default = (args) => {
-  return <Spacer {...args}></Spacer>
+const Box = ({ block, style }) => {
+  return (
+    <div
+      style={{
+        display: block ? 'block' : 'inline-block',
+        width: 100,
+        height: 100,
+        backgroundColor: 'blue',
+        ...style,
+      }}
+    />
+  )
+}
+
+export const Horizontal = (args) => {
+  return (
+    <Spacer {...args} type="horizontal">
+      <Box />
+      <Box />
+      <Box />
+    </Spacer>
+  )
+}
+
+export const Vertical = (args) => {
+  return (
+    <Spacer {...args} type="vertical">
+      <Box block />
+      <Box block />
+      <Box block />
+    </Spacer>
+  )
 }
